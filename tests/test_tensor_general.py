c65136ce8b5107250519c1a4576511b60e4536bb
Julia Yu <rubyjoycat@gmail.com>
2024-11-18 16:41:55 -0500

    3.2 works and passes tests

--- tests/test_tensor_general.py
+++ tests/test_tensor_general.py
@@ -306,7 +306,8 @@ if numba.cuda.is_available():
 
 
 @given(data())
-@settings(max_examples=26)
+@settings(max_examples=25)
+# @settings(suppress_health_check=(HealthCheck.data_too_large,))
 @pytest.mark.parametrize("fn", two_arg)
 @pytest.mark.parametrize("backend", backend_tests)
 def test_two_grad_broadcast(
