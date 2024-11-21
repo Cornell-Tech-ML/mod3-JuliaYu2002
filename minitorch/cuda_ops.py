a20df9b7d6a2dd300129cb11dd1e2d32261fe51f
Julia Yu <rubyjoycat@gmail.com>
2024-11-21 12:43:11 -0500

    putting comments ig

--- minitorch/cuda_ops.py
+++ minitorch/cuda_ops.py
@@ -402,17 +402,20 @@ def _mm_practice(out: Storage, a: Storage, b: Storage, size: int) -> None:
     pj = cuda.threadIdx.y
 
     acc = 0
-    for k in range(0, BLOCK_DIM, size):
-        if i < size and k + pj < size:
-            a_store_cache[pi, pj] = a[i, k + pj]
+    for k in range(0, size, BLOCK_DIM):
+        # from 0 to the size of the square, skipping by the threads per block, BLOCK_DIM
+        # (in order to cover each part of the storage and get each dot product using a given part of the data)
+        if i < size and k + pj < size: # guard against out of bounds (the column exceeding the size and the row exceeding the size)
+            a_store_cache[pi, pj] = a[i, k + pj] # place at the thread position, not the global position
         if j < size and k + pi < size:
             b_store_cache[pi, pj] = b[k + pi, j]
-        cuda.syncthreads()
+        cuda.syncthreads() # sync pause to get everything here
 
         for loc_k in range(min(BLOCK_DIM, size - k)):
-            acc += a_store_cache[pi, loc_k] * b_store_cache[loc_k, pj]
+            # loop through the smaller of the 2 values (BLOCK_DIM or size - k, representing the amount of numbers to dot product together in terms of dimension)
+            acc += a_store_cache[pi, loc_k] * b_store_cache[loc_k, pj] # move a across the rows, b down the column
 
-    if i < size and j < size:
+    if i < size and j < size: # if i, j is within the size of the out, write the accumulation to global
         out[i, j] = acc
 
 
